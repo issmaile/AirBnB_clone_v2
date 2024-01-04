0b91ac1fdf168da689931dc28b4465692718a150
issmaile <ismail25smello@gmail.com>
2024-01-04 23:49:37 +0100

    .

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+
+The application listens on 0.0.0.0:5000
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: displays "HBNB"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """display 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB()
+    """ display 'HBNB'"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
